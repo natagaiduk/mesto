941d237192181f990355d2b742352f80609e143f
Nataly Gaiduk <natagaiduk@inline-ltd.ru>
2023-04-05 22:21:26 +0300

    make bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,34 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let inputName = document.querySelector('.popup__field-name');
+let inputSubtitle = document.querySelector('.popup__field-subtitle');
+
+
 function openPopup() {
   popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputSubtitle.value = profileSubtitle.textContent;
 };
+
 editButton.addEventListener('click', openPopup);
 
 const closeButton = document.querySelector('.popup__close');
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
+
 closeButton.addEventListener('click', closePopup);
 
-let profileName = document.querySelector('.profile__name');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let inputName = document.querySelector('.popup__field-name');
-let inputSubtitle = document.querySelector('.popup__field-subtitle');
-inputName.value = profileName.textContent;
-inputSubtitle.value = profileSubtitle.textContent;
 
-
-let formElement = document.querySelector('.popup__form') 
 function formSubmitHandler(evt) {
   evt.preventDefault ();
-  let inputName = document.querySelector('.popup__field-name');
-  let inputSubtitle = document.querySelector('.popup__field-subtitle');
-  let profileName = document.querySelector('.profile__name');
-  let profileSubtitle = document.querySelector('.profile__subtitle');
   profileName.textContent = inputName.value;
   profileSubtitle.textContent = inputSubtitle.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
+let formElement = document.querySelector('.popup__form')
 
 formElement.addEventListener('submit', formSubmitHandler);
